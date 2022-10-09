b4f7cfe942e8a573b160f5fc2c1cfcb963449657
rondo <tnicodimt@gmail.com>
2022-10-09 22:02:15 +0300

    Basic chessboard
    Added a basic chessboard with ASCII symbols. No logic behind placing rooks yet.

--- 8_rooks/chess_main.cpp
+++ 8_rooks/chess_main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+const char BLACK = 219;
+const char WHITE = 32;
 
 int main() {
-
+	int i, j, k = 0;
+	for (i = 1; i <= 8; i++)
+	{
+		for (j = 1; j <= 8; j++)
+		{
+			if (i == 2 && j == 2) {
+				cout << "";
+			}
+			if ((i + j) % 2 == 0)
+				cout << BLACK << BLACK << BLACK;
+			else {
+				if (k % 2 == 0) {
+					cout << WHITE << WHITE << WHITE;
+				}
+				else {
+					cout << WHITE << "R" << WHITE;
+				}
+				k++;
+			}
+		}
+		printf("\n");
+	}
+	_getch();
 	return 0;
 }
