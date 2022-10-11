85517fd2439c2f655ff81d225d36d76558a2b3e7
rondo <tnicodimt@gmail.com>
2022-10-11 21:05:30 +0300

    8Rooks v1.0
    Fully functional 8 rooks problem with user input & ASCII art chessboard

--- 8_rooks/chess_main.cpp
+++ 8_rooks/chess_main.cpp
@@ -17,6 +17,8 @@ void initBoard(int x,int y) {
 			board[i][j] = 0;
 			init++;
 		}
+	if (x == -1 || y == -1)
+		return;
 	//adds rooks(= 1) and squares under attack(= 2)
 		board[x][y] = 1;
 		for (int i = 0; i < 8; i++)
@@ -25,16 +27,6 @@ void initBoard(int x,int y) {
 		for (int j = 0; j < 8; j++)
 			if (!board[x][j])
 				board[x][j] = 2;
-}
-
-void printMatrix() {
-	cout << "\n\n";
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			cout << board[i][j] << ' ';
-		}
-		cout << endl;
-	}
 }
 
 void printBoard() {
@@ -78,19 +70,25 @@ int isValid(int x,int y) {
 	return 1;
 }
 
+void placeRooks(){
+	for (int i = 0; i < 8; i++)
+		for (int j = 0; j < 8; j++)
+			if (board[i][j] == 0)
+				initBoard(i, j);
+}
 
-void initMain() {
-	int x, y, input = 1, opt;
-	while (input) {
+int main() {
+	int x, y, opt;
+	while (1) {
 		printBoard();
-		cout << "1. Add a new rook\n2. Exit input stage\nOption: ";
+		cout << "1. Add a new rook\n2. Automatic rook placement\n3. Restart board\nOption: ";
 		cin >> opt;
 		char position[5];
 		int ok = 0;
 		switch (opt) {
 		case 1:
 			while (!ok) {
-				cout << "Choose a square using standard chess notation (ex: a3,c7 etc.): ";
+				cout << "Choose a square using standard chess notation (ex: a4, f7 etc.): ";
 				cin >> position;
 				x = 8 - (int)(position[1] - 48);
 				y = position[0] - 97;
@@ -105,20 +103,22 @@ void initMain() {
 				}
 			}
 			initBoard(x, y);
-			printMatrix();
 			break;
 		case 2:
-			input = 0;
+			placeRooks();
+			printBoard();
+			break;
+		case 3:
+			init = 0;
+			initBoard(-1, -1);
+			break;
+		case 0:
+			exit(0);
 			break;
 		default:
 			cout << "Invalid choice! Try again!\n";
 			break;
 		}
 	}
-}
-
-int main() {
-	initMain();
-	_getch();
 	return 0;
 }
